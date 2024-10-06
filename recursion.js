f15fa3304a8e4e00e8b2068b582cb9b4350afc1d
Mridhul <mridhul67@gmail.com>
2024-10-06 20:41:45 +0530

    Write function to return an array of fibonacci sequence recursively

--- recursion.js
+++ recursion.js
@@ -12,3 +12,16 @@ function fibs(length) {
 }
 
 console.log(fibs(8));
+
+function fibsRec(length) {
+  //recursive method
+  console.log("This was printed recursively"); //to count number of times function was called recursively
+  if (length === 1) {
+    return [0, 1];
+  }
+  let num = fibsRec(length - 1);
+  num.push(num[num.length - 1] + num[num.length - 2]);
+  return num;
+}
+
+console.log(fibsRec(8));
